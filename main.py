a07be06c62bc94d36ff60e575edff94d38ec9b33
tharshan <tharshanthamizlan@gmail.com>
2025-01-17 10:08:38 +0530

    whatsapp, contact us

--- main.py
+++ main.py
@@ -272,9 +272,9 @@ def logout():
     flash('Logged out successfully.', 'success')
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
-
 # if __name__ == '__main__':
-#     app.run(debug=True)
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port, debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
